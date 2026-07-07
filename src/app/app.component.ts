075c6e235e21be33b9af2bb17dcd24ee4fb04bc3
thomue100 <46538287+thomue100@users.noreply.github.com>
2026-07-07 21:55:08 +0200

    Vercel Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ControlsComponent } from './features/controls/controls.component';
 import { CanvasComponent } from './features/canvas/canvas.component';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { CanvasComponent } from './features/canvas/canvas.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  ngOnInit() {
+    // Initialisiert Analytics beim Starten der Anwendung
+    inject();
+  }
+}
